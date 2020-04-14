7043ab362a8bcae0c148b738fbc5d5a960680def
Karolina Podczasi <karolina.podczasi@gmail.com>
2020-04-14 00:02:20 -0400

    update index and script

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
+
 var newComputerMove, argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
 var refreshPage, buttonClear, argButtonName, buttonRock, buttonPaper, buttonScissors;
 
 function buttonClicked (argButtonName) { 
     clearMessages (); 
-    console.log (argButtonName + 'has been clicked'); }
+    console.log (argButtonName + ' has been clicked'); }
 function refreshPage(){
     window.location.reload();
 } 
@@ -13,25 +14,13 @@ buttonRock = document.getElementById ('button-rock');
 buttonPaper = document.getElementById ('button-paper'); 
 buttonScissors = document.getElementById ('button-scissors'); 
 
-// randomNumber = Math.floor (Math.random () * 3 + 1);
-// console.log ('the drawn number is:' + randomNumber);
-// if (randomNumber == '1') {computerMove = 'stone';} 
-// else if (randomNumber == '2') {computerMove = 'paper';}
-// else if (randomNumber == '3') {computerMove = 'scissors';}
-// else {computerMove = 'unknown traffic';}
+// console.log('the ansewer was' + playerInput);
+// if (playerInput== buttonRock) { playerMove= 'stone';}
+// else if (playerInput==buttonPaper) {playerMove='paper';}
+// else if(playerInput==buttonScissors) {playerMove='scissors';}
+// else {playerMove= 'unknown traffic';}
 
 
-// printMessage ('My move:' + computerMove);
-
-
-// playerInput = prompt ('choose your move, lad!: 1 for stone, 2 for paper, 3 for scissors! 😁')
-console.log('the ansewer was' + playerInput);
-if (playerInput== buttonRock) { playerMove= 'stone';}
-else if (playerInput==buttonPaper) {playerMove='paper';}
-else if(playerInput==buttonScissors) {playerMove='scissors';}
-else {playerMove= 'unknown traffic';}
-
-// printMessage('your move '+ playerMove);
 
 function getMoveName (argMoveId) 
 {
@@ -44,105 +33,25 @@ function getMoveName (argMoveId)
 
 function displayResult(playerMove, computerMove) 
 {
-    console.log ('creating displayResult funtion wuth arguements' + playerMove + computerMove);
+    console.log ('creating displayResult function with arguements: ' + playerMove + ' and ' + computerMove);
     if(playerMove =='stone' && computerMove =='scissors') {printMessage('Win');}
     else if(playerMove=='paper' && computerMove == 'stone') {printMessage('Win');}
     else if(playerMove=='scissors' && computerMove == 'paper') {printMessage('Win');}
     else if(computerMove==playerMove) {printMessage('Tie');}
     else {printMessage ('Looser!')}
-    printMessage('Computer has choosen: ' + computerMove + ' and player has chosen: ' + playerMove );
+    printMessage('Computer has choosen: ' + computerMove + ' and player has choosen: ' + playerMove );
 }
 
-
-
 playerMove = argButtonName
-// playerInput = prompt ('Choose your move! 1: stone, 2: paper, 3: scissors.');
-// console.log ('player has chosen:' + playerInput);
-// playerMove = getMoveName (playerInput);
 
-// console.log ('player has chosen: ' + playerMove);
 randomNumber = Math.floor (Math.random () * 3 + 1);
 console.log ('the drawn number is:' + randomNumber);
 computerMove = getMoveName (randomNumber);
-console.log ('computer has choosen:' + computerMove);
+console.log ('computer has choosen: ' + computerMove);
 
 
-buttonClear.addEventListener ('click', function () {buttonClicked ('Reset Button');});
-buttonRock.addEventListener ('click', function () {buttonClicked  ('stone')  +  displayResult ('stone', computerMove);});
-buttonPaper.addEventListener ('click', function () {buttonClicked  ('paper')  + displayResult ('paper', computerMove)});
-buttonScissors.addEventListener ('click', function () {buttonClicked  ('scissors')  + displayResult ('scissors', computerMove)});
-
-var newComputerMove, argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
-var refreshPage, buttonClear, argButtonName, buttonRock, buttonPaper, buttonScissors;
-
-function buttonClicked (argButtonName) { 
-    clearMessages (); 
-    console.log (argButtonName + 'has been clicked'); }
-function refreshPage(){
-    window.location.reload();
-} 
-
-buttonClear = document.getElementById ('button-clear'); 
-buttonRock = document.getElementById ('button-rock'); 
-buttonPaper = document.getElementById ('button-paper'); 
-buttonScissors = document.getElementById ('button-scissors'); 
-
-// randomNumber = Math.floor (Math.random () * 3 + 1);
-// console.log ('the drawn number is:' + randomNumber);
-// if (randomNumber == '1') {computerMove = 'stone';} 
-// else if (randomNumber == '2') {computerMove = 'paper';}
-// else if (randomNumber == '3') {computerMove = 'scissors';}
-// else {computerMove = 'unknown traffic';}
-
-
-// printMessage ('My move:' + computerMove);
-
-
-// playerInput = prompt ('choose your move, lad!: 1 for stone, 2 for paper, 3 for scissors! 😁')
-console.log('the ansewer was' + playerInput);
-if (playerInput== buttonRock) { playerMove= 'stone';}
-else if (playerInput==buttonPaper) {playerMove='paper';}
-else if(playerInput==buttonScissors) {playerMove='scissors';}
-else {playerMove= 'unknown traffic';}
-
-// printMessage('your move '+ playerMove);
-
-function getMoveName (argMoveId) 
-{
-    console.log ('creating getMoveName function with' + argMoveId);
-    if (argMoveId == '1') {return 'stone'}
-    else if (argMoveId == '2') {return 'paper'}
-    else if (argMoveId == '3') {return 'scissors'}
-    else {printMessage ('I do not understand this id ' + argMoveId + 'I assume you meant stone'); return 'stone'}
-}
-
-function displayResult(playerMove, computerMove) 
-{
-    console.log ('creating displayResult funtion wuth arguements' + playerMove + computerMove);
-    if(playerMove =='stone' && computerMove =='scissors') {printMessage('Win');}
-    else if(playerMove=='paper' && computerMove == 'stone') {printMessage('Win');}
-    else if(playerMove=='scissors' && computerMove == 'paper') {printMessage('Win');}
-    else if(computerMove==playerMove) {printMessage('Tie');}
-    else {printMessage ('Looser!')}
-    printMessage('Computer has choosen: ' + computerMove + ' and player has chosen: ' + playerMove );
-}
-
-
-
-playerMove = argButtonName
-// playerInput = prompt ('Choose your move! 1: stone, 2: paper, 3: scissors.');
-// console.log ('player has chosen:' + playerInput);
-// playerMove = getMoveName (playerInput);
-
-// console.log ('player has chosen: ' + playerMove);
-randomNumber = Math.floor (Math.random () * 3 + 1);
-console.log ('the drawn number is:' + randomNumber);
-computerMove = getMoveName (randomNumber);
-console.log ('computer has choosen:' + computerMove);
-
-
-buttonClear.addEventListener ('click', function () {buttonClicked ('Reset Button');});
-buttonRock.addEventListener ('click', function () {buttonClicked  ('stone')  +  displayResult ('stone', computerMove);});
+buttonClear.addEventListener ('click', function () {buttonClicked ('Reset Button');}); computerMove = getMoveName (randomNumber);
+buttonRock.addEventListener ('click', function () {buttonClicked  ('stone');  +  displayResult ('stone', computerMove);});
 buttonPaper.addEventListener ('click', function () {buttonClicked  ('paper')  + displayResult ('paper', computerMove)});
 buttonScissors.addEventListener ('click', function () {buttonClicked  ('scissors')  + displayResult ('scissors', computerMove)});
 
